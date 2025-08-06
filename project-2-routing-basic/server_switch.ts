607d36c8a7a5e943e2458a7d5b81452d636b91d9
faheemnaveed07 <faheemnaveed09@gmail.com>
2025-08-07 00:44:32 +0500

    adding file, routing with switch statement

---
+++ project-2-routing-basic/server_switch.ts
@@ -0,0 +1,30 @@
+async function handler(req:Request){
+  const url=new URL(req.url);
+  const path=url.pathname;
+  console.log("Request Received from path:", path);
+  switch(path)
+  {
+    case"/":
+    return new Response("Welcome to the Home Page!",{headers:{"content-type":"text/plain"}});
+    case "/about":
+      return new Response("Welcome to the About Page!",{headers:{"content-type":"text/plain"}});
+      case "/contact":
+        return new Response("Welcome to the Contact Page!",{headers:{"content-type":"text/plain"}});
+        case "/api/products":{
+          const products=[
+            {id:1,name:"Laptop",price:1000},
+            {id:2,name:"Smartphone",price:500},
+            {id:3,name:"Tablet",price:300}
+          ];
+          return new Response(JSON.stringify(products),{headers:{"content-type":"application/json"}});}
+          case "/api/users":{
+            const users=[
+              {id:1,name:"Faheem",email:"example@gmail.com"},
+              {id:2,name:"Qamar",email:"example1@gmail.com"}
+            ];
+            return new Response(JSON.stringify(users),{headers:{"content-types":"application/json"}});}
+          default:
+            return new Response("Not found",{headers:{"content-type":"text/plain"},status:404});
+  }
+// deno-lint-ignore no-unreachable
+Deno.serve({handler});
